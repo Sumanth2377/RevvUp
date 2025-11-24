078626d3b5312665ead6b40744e7eca70bab9e07
Sumanth K S <sumanth10ks@gmail.com>
2025-11-24 08:56:12 +0000

    host this whole web app in vercel give steps and start doing

--- next.config.ts
+++ next.config.ts
@@ -31,6 +31,12 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  env: {
+    NEXT_PUBLIC_FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    NEXT_PUBLIC_FIREBASE_PROJECT_ID: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    NEXT_PUBLIC_FIREBASE_APP_ID: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  }
 };
 
 export default nextConfig;
--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,6 @@
 export const firebaseConfig = {
-  "projectId": "studio-1738767715-e5992",
-  "appId": "1:316165834156:web:ab90b30251a8979d9c6609",
-  "apiKey": "AIzaSyBIzzN61gPmjfGoTnxvahTz0Ha_Sn9tJaI",
-  "authDomain": "studio-1738767715-e5992.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "316165834156"
+  "projectId": process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  "appId": process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  "authDomain": process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
 };
